e3d658913322957230e15a2cb4e2eb48f7bcda1c
erwinnunez <er.nunez@duocuc.cl>
2023-08-28 13:52:02 -0400

    footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EncabezadosComponent } from './encabezados/encabezados.component';
 import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [EncabezadosComponent],
-  exports: [EncabezadosComponent],
+  declarations: [EncabezadosComponent, FooterComponent],
+  exports: [EncabezadosComponent, FooterComponent],
   imports: [
     CommonModule,
     IonicModule
